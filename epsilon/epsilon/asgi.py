6f5ef7ca64c47e20924ad0a6b1e8353604fb7ac7
lilvedaes <daventuro@gmail.com>
2021-06-06 20:41:18 -0500

    EP-37 and EP-39 Django project setup: create a basic view and setup database connection

---
+++ epsilon/epsilon/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for epsilon project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'epsilon.settings')
+
+application = get_asgi_application()

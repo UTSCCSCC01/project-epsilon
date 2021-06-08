7a28b0dd2165bc05a28c22d7b241f9350ebcb506
Hritik Gandhi <hritikgandhi99@gmail.com>
2021-06-08 17:33:40 -0400

    EP-52 Migrated from Django to Flask
    Added basic framework and removed older files.

---
+++ epsilon/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'MyDB'
+
+mysql = MySQL(app)
+
+@app.route("/")
+def hello():
+  return "Hello World! Welcome to Epsilon!"
+
+if __name__ == "__main__":
+  app.run()

d7f2068172a6db4d07643227e65f070fcb6e191d
Hritik Gandhi <hritikgandhi99@gmail.com>
2021-06-08 23:40:08 -0400

    EP-52 [General Improvement] Added method to add data to database with string query and tuple dataset.
    Example:
        str = '''qry %s'''.
        data = (x).
        Where %s will be replaced by x.
    
    Modified populate to also add a Teams table.

--- epsilon/populatedatabase.py
+++ epsilon/populatedatabase.py
@@ -1,13 +1,25 @@
 from flask import Flask
 from flask_mysqldb import MySQL
 
+
 def populate(mysql):
-    #Create a table with 5 users. 2 admin and 3 normal users
+    # Create a table with 5 users. 2 admin and 3 normal users
     cur = mysql.connection.cursor()
     cur.execute('''CREATE TABLE Users (uid INTEGER, role INTEGER)''')
+    cur.execute(
+        '''CREATE TABLE Teams (tid INTEGER, uid INTEGER, role INTEGER)''')
     cur.execute('''INSERT INTO Users VALUES (1, 1)''')
     cur.execute('''INSERT INTO Users VALUES (2, 1)''')
     cur.execute('''INSERT INTO Users VALUES (3, 0)''')
     cur.execute('''INSERT INTO Users VALUES (4, 0)''')
     cur.execute('''INSERT INTO Users VALUES (5, 0)''')
     mysql.connection.commit()
+    return
+
+
+def add_data(mysql, sql_q, data):
+    # Adds data to table.
+    cur = mysql.connection.cursor()
+    cur.execute(sql_q, data)
+    mysql.connection.commit()
+    return "Added data successfully."

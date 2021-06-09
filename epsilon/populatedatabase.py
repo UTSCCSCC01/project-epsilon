00c3cbefad956ff7d56d84eef91d9ef8600a022f
Weiyu Li <weiyu.li@mail.utoronto.ca>
2021-06-09 00:43:02 -0400

    add error handling for /create and /manageteam

--- epsilon/populatedatabase.py
+++ epsilon/populatedatabase.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask_mysqldb import MySQL
 
-
 def populate(mysql):
     # Create a table with 5 users. 2 admin and 3 normal users
     cur = mysql.connection.cursor()
-    cur.execute('''CREATE TABLE Users (uid INTEGER, role INTEGER)''')
+    cur.execute('''CREATE TABLE IF NOT EXISTS Users (uid INTEGER, role INTEGER)''')
     cur.execute(
-        '''CREATE TABLE Teams (tid INTEGER, uid INTEGER, role INTEGER)''')
+        '''CREATE TABLE IF NOT EXISTS Teams (tid INTEGER, uid INTEGER, role INTEGER)''')
     cur.execute('''INSERT INTO Users VALUES (1, 1)''')
     cur.execute('''INSERT INTO Users VALUES (2, 1)''')
     cur.execute('''INSERT INTO Users VALUES (3, 0)''')
@@ -15,7 +14,6 @@ def populate(mysql):
     cur.execute('''INSERT INTO Users VALUES (5, 0)''')
     mysql.connection.commit()
     return
-
 
 def add_data(mysql, sql_q, data):
     # Adds data to table.

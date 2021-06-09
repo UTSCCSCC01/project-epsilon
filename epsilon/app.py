165c9bf5fdaadf6263a8558a07f8733e50c4d368
Hritik Gandhi <hritikgandhi99@gmail.com>
2021-06-08 23:44:45 -0400

    EP-29 [Feature] Back end for team management.
    Setup basic POST and GET for manageteam. Basic form that can add values to the database with a simple check if empty condition.

--- epsilon/app.py
+++ epsilon/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from flask_mysqldb import MySQL
-from populatedatabase import populate
+from populatedatabase import populate, add_data
 
 app = Flask(__name__)
 
@@ -11,15 +11,41 @@ app.config['MYSQL_DB'] = 'epsilon_db'
 
 mysql = MySQL(app)
 
+
 @app.route("/")
 def hello():
-  return "Hello World! Welcome to Epsilon!"
+    return "Hello World! Welcome to Epsilon!"
 
 # Only go to this page if your database is empty
+
+
 @app.route("/create")
 def create():
-  populate(mysql)
-  return "Database is populated!"
+    populate(mysql)
+    return "Database is populated!"
+
+# EP-1: Team management
+
+
+@app.route('/manageteam', methods=['GET', 'POST'])
+def manageteam():
+    # TODO: check if user is logged in and check permissions
+    cur = mysql.connection.cursor()
+    if request.method == 'POST':
+        # create both queries for checking and inserting data
+        sql_q = '''INSERT INTO Teams VALUES (%s, %s, 1)'''
+        # check if all form boxes are completed
+        if (len(request.form['teamid']) == 0 or len(request.form['userid']) == 0):
+            error = 'Please fill in all boxes.'
+            return render_template('manageteam.html', error=error)
+        # if no errors
+        message = add_data(
+            mysql, sql_q, (request.form['teamid'], request.form['userid']))
+        return render_template('manageteam.html', message=message)
+    else:
+        # load if not POST
+        return render_template("manageteam.html")
+
 
 if __name__ == "__main__":
-  app.run()
+    app.run(debug=True)

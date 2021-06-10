b136ad94e77b4588bc9b6501b2c1134ec36b4559
Hritik Gandhi <hritikgandhi99@gmail.com>
2021-06-10 18:26:26 -0400

    [EP-1] [Bug Fix] Updated back end database to match schema and made changes to the name.

--- epsilon/registration.py
+++ epsilon/registration.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, render_template
-from populatedatabase import add_data
+from populatedatabase import add_data, get_data
 
-# EP-1: Team management
 def is_pos_int(s):
+    # EP-1: Team management
     try:
         if int(s)>0:
             return True
@@ -11,28 +11,32 @@ def is_pos_int(s):
     except ValueError:
         return False
 
+def check_team_exists(mysql, name):
+    # Returns 1 if match is found else returns 0
+    companies = get_data(mysql, "Company")
+    for item in companies:
+        if (item[1].lower() == name.lower()):
+            return 1
+    return 0
+
 def registration(mysql):
     # TODO: check if user is logged in and check permissions
     # cur = mysql.connection.cursor()
     if request.method == 'POST':
         # create both queries for checking and inserting data
-        sql_q = '''INSERT INTO Teams VALUES (%s, %s, 1)'''
+        sql_q = '''INSERT INTO Company (name, description) VALUES (%s, %s)'''
         # check if all form boxes are completed
-        if (len(request.form['teamid']) == 0 or len(request.form['userid']) == 0):
+        if (len(request.form['teamname']) == 0 or len(request.form['teamdesc']) == 0):
             error = 'Please fill in all boxes.'
             return render_template('registration.html', error=error)
-        # check if all form boxes are integers
-        if not (is_pos_int(request.form['teamid']) and  is_pos_int(request.form['userid'])):
-            error = 'team id and user id are in digit format'
-            return render_template('registration.html', error=error)
-        # check if input int is out of range of mySql, just for demo purpose
-        if int(request.form['teamid'])>2147483647 or int(request.form['userid'])>2147483647:
-            error = 'Id number is too large'
+        # If the company exists.
+        if (check_team_exists(mysql, request.form['teamname']) == 1):
+            error = 'This company already exists. Please try with a new name or request to join ' + request.form['teamname'] + ' here.'
             return render_template('registration.html', error=error)
         # if no errors
         try:
             message = add_data(
-                mysql, sql_q, (request.form['teamid'], request.form['userid']))
+                mysql, sql_q, (request.form['teamname'], request.form['teamdesc']))
         except Exception as e:
             return render_template('registration.html', error=e)
         return render_template('registration.html', message=message)

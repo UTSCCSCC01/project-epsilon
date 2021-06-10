454eda35404b7380ee6322be53fb762355a9409e
Hritik Gandhi <hritikgandhi99@gmail.com>
2021-06-10 14:29:52 -0400

    EP-1 [Refractor] Moved back end code to its own file for better readability.

--- epsilon/app.py
+++ epsilon/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 from flask_mysqldb import MySQL
 from populatedatabase import populate, add_data
+from registration import registration
 
 app = Flask(__name__)
 
@@ -25,7 +26,6 @@ def create():
     return "Database Users, Teams are populated!"
 
 # EP-1: Team management
-
 def is_pos_int(s):
     try:
         if int(s)>0:
@@ -36,34 +36,8 @@ def is_pos_int(s):
         return False
 
 @app.route('/registration', methods=['GET', 'POST'])
-def registration():
-    # TODO: check if user is logged in and check permissions
-    # cur = mysql.connection.cursor()
-    if request.method == 'POST':
-        # create both queries for checking and inserting data
-        sql_q = '''INSERT INTO Teams VALUES (%s, %s, 1)'''
-        # check if all form boxes are completed
-        if (len(request.form['teamid']) == 0 or len(request.form['userid']) == 0):
-            error = 'Please fill in all boxes.'
-            return render_template('registration.html', error=error)
-        # check if all form boxes are integers
-        if not (is_pos_int(request.form['teamid']) and  is_pos_int(request.form['userid'])):
-            error = 'team id and user id are in digit format'
-            return render_template('registration.html', error=error)
-        # check if input int is out of range of mySql, just for demo purpose
-        if int(request.form['teamid'])>2147483647 or int(request.form['userid'])>2147483647:
-            error = 'Id number is too large'
-            return render_template('registration.html', error=error)
-        # if no errors
-        try:
-            message = add_data(
-                mysql, sql_q, (request.form['teamid'], request.form['userid']))
-        except Exception as e:
-            return render_template('registration.html', error=e)
-        return render_template('registration.html', message=message)
-    else:
-        # load if not POST
-        return render_template("registration.html")
+def reg():
+    return registration(mysql)
 
 
 if __name__ == "__main__":
---
+++ epsilon/registration.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, render_template
+from populatedatabase import add_data
+
+# EP-1: Team management
+def is_pos_int(s):
+    try:
+        if int(s)>0:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+
+def registration(mysql):
+    # TODO: check if user is logged in and check permissions
+    # cur = mysql.connection.cursor()
+    if request.method == 'POST':
+        # create both queries for checking and inserting data
+        sql_q = '''INSERT INTO Teams VALUES (%s, %s, 1)'''
+        # check if all form boxes are completed
+        if (len(request.form['teamid']) == 0 or len(request.form['userid']) == 0):
+            error = 'Please fill in all boxes.'
+            return render_template('registration.html', error=error)
+        # check if all form boxes are integers
+        if not (is_pos_int(request.form['teamid']) and  is_pos_int(request.form['userid'])):
+            error = 'team id and user id are in digit format'
+            return render_template('registration.html', error=error)
+        # check if input int is out of range of mySql, just for demo purpose
+        if int(request.form['teamid'])>2147483647 or int(request.form['userid'])>2147483647:
+            error = 'Id number is too large'
+            return render_template('registration.html', error=error)
+        # if no errors
+        try:
+            message = add_data(
+                mysql, sql_q, (request.form['teamid'], request.form['userid']))
+        except Exception as e:
+            return render_template('registration.html', error=e)
+        return render_template('registration.html', message=message)
+    else:
+        # load if not POST
+        return render_template("registration.html")

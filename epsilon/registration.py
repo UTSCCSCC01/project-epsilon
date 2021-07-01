ac87e35f17e2965e8e1d803efaa5cd65dc0db22e
Hritik Gandhi <hritikgandhi99@gmail.com>
2021-07-01 15:59:45 -0400

    [EP-60] Modified registration to display and update industry information.

--- epsilon/registration.py
+++ epsilon/registration.py
@@ -29,27 +29,29 @@ def check_team_exists(dao, name):
 def registration(dao):
     # TODO: check if user is logged in and check permissions
     # cur = mysql.connection.cursor()
+    industry = dao.get_industry()
     if request.method == 'POST':
         # check if all form boxes are completed
-        if (len(request.form['teamname']) == 0 or len(request.form['teamdesc']) == 0):
+        if (len(request.form['teamname']) == 0 or len(request.form['teamdesc']) == 0 or len(request.form['industryselect']) == 0):
             error = 'Please fill in all boxes.'
-            return render_template('registration.html', error=error)
+            return render_template('registration.html', error=error, industry=industry)
         # If the company exists.
         if (check_team_exists(dao, request.form['teamname']) == 1):
             error = 'This company already exists. Please try with a new name or request to join ' + request.form['teamname'] + ' here.'
-            return render_template('registration.html', error=error)
+            return render_template('registration.html', error=error, industry=industry)
         # if no errors
         try:
             company = Company(name=request.form['teamname'],
-                              description=request.form['teamdesc'])
-            message = dao.add_company(company)
+                              description=request.form['teamdesc'],ind_id=request.form['industryselect'])
+            dao.add_company(company)
             joe = User(rid=Role.TEAM_OWNER.value, name="Joe", contact="Jo@gmail.com")
             team = Team(tid=3, uid=6, rid=Role.TEAM_OWNER.value)
             dao.add_user(joe)
             dao.add_team(team)
+            message = "Registered!"
         except Exception as e:
             return render_template('registration.html', error=e)
-        return render_template('registration.html', message=message)
+        return render_template('registration.html', message=message, industry=industry)
     else:
         # load if not POST
-        return render_template("registration.html")
+        return render_template("registration.html", industry=industry)

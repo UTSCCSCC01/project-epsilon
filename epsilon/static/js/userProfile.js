8ebcdced0f9a468df920c3b5b040f55ab6bb8280
adin65 <46980135+adin65@users.noreply.github.com>
2021-07-02 01:38:16 -0400

    [EP-21][Feature] Add edit and save button to userprofile.

--- epsilon/static/js/userProfile.js
+++ epsilon/static/js/userProfile.js
@@ -1,31 +1,48 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
-// import EditBtn from './editBtn';
+// // import EditBtn from './editBtn';
 
-class EditBtn extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isEditing: false
-        }
+// class EditBtn extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             isEditing: false,
+//         }
+//     };
+
+//     handleClick() {
+//         isEditing = this.state.isEditing;
+//         this.setState({
+//             isEditing: !isEditing,
+//         });
+//     };
+
+//     render() {
+//         const saveOrEdit = isEditing ? "Save" : "Edit";
+//         return (
+//             <button type="submit">{saveOrEdit}</button>
+//         )
+//     }
+// }
+
+//     ReactDOM.render(
+//         <EditBtn />,
+//         document.getElementById("editBtn")
+//     );
+
+var is_editing = false
+function isEditing() {
+    is_editing = !is_editing;
+    console.log(is_editing);
+
+    const pencil_icon = '<i class="fa fa-pencil" aria-hidden="true""></i> ';
+    let display_name = is_editing ? "Save" : "Edit";
+    document.getElementById("editBtn").innerHTML = pencil_icon + display_name;
+
+    document.getElementById('name').readOnly = !is_editing;
+    document.getElementById('description').readOnly = !is_editing;
+    document.getElementById('contact').readOnly = !is_editing;
+    if (!is_editing) {
+        document.getElementById("userForm").submit();
     }
-
-    handleClick() {
-        isEditing = this.state.isEditing;
-        this.setState({
-            isEditing: !isEditing,
-        });
-    }
-
-    render() {
-        const saveOrEdit = isEditing ? "Save" : "Edit"
-        return (
-            <button type="submit">{saveOrEdit}</button>
-        )
-    }
-};
-
-    ReactDOM.render(
-        <EditBtn />,
-        document.getElementById("editBtn")
-    );
+}

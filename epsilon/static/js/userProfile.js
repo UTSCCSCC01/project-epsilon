b8acecc572c1bcf73040b30c846840de3977c809
adin65 <46980135+adin65@users.noreply.github.com>
2021-07-02 09:41:17 -0400

    [EP-21][Bug Fix] Fix discard changes button and edit/save button.

--- epsilon/static/js/userProfile.js
+++ epsilon/static/js/userProfile.js
@@ -1,6 +1,5 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
-// // import EditBtn from './editBtn';
 
 // class EditBtn extends React.Component {
 //     constructor(props) {
@@ -32,18 +31,41 @@
 
 var is_editing = false
 function isEditing() {
-    is_editing = !is_editing;
-    console.log(is_editing);
+    //toggle edit/save button
+    const valid = document.getElementById("userForm").checkValidity();
 
+    if (!valid) {
+        document.getElementById("validationMsg").innerHTML = "A field is not valid, please try again.";
+    } else {
+        is_editing = !is_editing;
+
+        if (!is_editing) {
+            document.getElementById("userForm").submit();
+        }
+
+        updateEditDisplay();
+    }
+
+}
+
+function disableEdit() {
+    is_editing = false;
+    updateEditDisplay();
+}
+
+function updateEditDisplay() {
+    // switch between editable/readonly fields 
+    
+    document.getElementById("responseMsg").innerHTML = "";
+    
     const pencil_icon = '<i class="fa fa-pencil" aria-hidden="true""></i> ';
     let display_name = is_editing ? "Save" : "Edit";
+
     document.getElementById("editBtn").innerHTML = pencil_icon + display_name;
     document.getElementById("discardBtn").style.display = is_editing ? "inline" : "none";
 
     document.getElementById('name').readOnly = !is_editing;
     document.getElementById('description').readOnly = !is_editing;
     document.getElementById('contact').readOnly = !is_editing;
-    if (!is_editing) {
-        document.getElementById("userForm").submit();
-    }
 }
+

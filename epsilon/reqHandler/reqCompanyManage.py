e10c5095c2e3197412859c4994195dd40a6c06e1
Hritik Gandhi <hritikgandhi99@gmail.com>
2021-07-11 20:57:10 -0400

    EP-7 Added ability to edit company information.

--- epsilon/reqHandler/reqCompanyManage.py
+++ epsilon/reqHandler/reqCompanyManage.py
@@ -4,13 +4,20 @@ from flask import request, render_template, redirect, url_for
 
 def render_company_profile(mysql: MySQL, tid: int):
     """
-    Handler for company profile.
+    Handler for user profile.
     :param mysql: mysql db.
     :param tid: tid of company
     :return template for company profile page.
     """
     message = ""
     try:
+        if request.method == 'POST':
+            data = request.get_json
+            if data:
+                name = request.form["name"]
+                description = request.form["description"]
+                message = update_company(mysql, tid, name,
+                                         description)
         company_details = get_company_profile(mysql, tid)
         return render_template('company_profile.html', company_details=company_details,
                                message=message)

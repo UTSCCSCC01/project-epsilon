3046f23e508aa571a11d175c851afe8a47b43b05
Hritik Gandhi <hritikgandhi99@gmail.com>
2021-07-16 14:01:12 -0400

    Renamed search test to search_page

--- epsilon/reqHandler/reqSearch.py
+++ epsilon/reqHandler/reqSearch.py
@@ -14,13 +14,13 @@ def render_company_search(mysql: MySQL):
         try:
             search = request.form['search']
             data, message = company_search(mysql, search)
-            return render_template('search_test.html', message=message,
+            return render_template('search_page.html', message=message,
                                    data=data)
         except Exception as e:
             error_json = json.dumps({"message": str(e)})
-            return render_template('search_test.html', error=error_json)
+            return render_template('search_page.html', error=error_json)
     else:
-        return render_template("search_test.html")
+        return render_template("search_page.html")
 
 
 # related to testing frontend, won't interfere with back end
@@ -57,10 +57,10 @@ def search_frontend_test(succeed=True):
     print("search_frontend_test")
     if request.method == 'POST':
         if succeed:
-            return render_template("search_test.html",
+            return render_template("search_page.html",
                                    data=generate_search_result())
         else:
-            return render_template("search_test.html",
+            return render_template("search_page.html",
                                    error=generate_error_data())
     else:
-        return render_template("search_test.html")
+        return render_template("search_page.html")

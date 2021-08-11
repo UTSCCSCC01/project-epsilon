f63f4f0dc514b5d7c2c1590a948f1efc6f86866b
Kobe Louis <kobelouis123@gmail.com>
2021-08-11 08:42:50 -0400

    Reverted "your company" fix
    The fix for "your company" showing for people without companies broke the company page for people not in companies

--- epsilon/reqHandler/reqCompanyManage.py
+++ epsilon/reqHandler/reqCompanyManage.py
@@ -1,5 +1,5 @@
 from epsilonModules.ModCompany import *
-from epsilonModules.ModTeam import get_user_role, get_user_teams
+from epsilonModules.ModTeam import get_user_teams
 from flask import request, render_template
 from flask_login import current_user
 from classes.Role import Role
@@ -19,37 +19,31 @@ def render_company_profile(mysql: MySQL, name:str=""):
         # if name is not given, (endpoint is /yourcompany)
         # we need the user to belong to a company to determine
         # which company to display
-        if current_user.is_authenticated:
+        if name == "" and current_user.is_authenticated:
             user_teams = get_user_teams(mysql, current_user.uid)
             user_team = [user_teams[0].tid,
                          user_teams[0].uid,
                          user_teams[0].rid]
-        
-        if name == "":
-            tid = user_team[0]
-            target_tid = tid
+            tid = user_teams[0].tid
             rid = user_teams[0].rid
         else:   # if name is given or current_user is not logged in
             company_details = get_company_profile_by_name(mysql, name)
-            target_tid = company_details.tid
-            tid = user_team[0]
-            if current_user.is_authenticated:
-                rid = get_user_role(mysql, tid, current_user.uid)
+            tid = company_details.tid
 
         if request.method == 'POST':
             data = request.get_json
             if data:
                 name = request.form["name"]
                 description = request.form["description"]
-                message = update_company(mysql, target_tid, name,
+                message = update_company(mysql, tid, name,
                                          description)
-        company_details = get_company_profile(mysql, target_tid)
-        company_owner = get_company_owner_by_tid(mysql, target_tid)
+        company_details = get_company_profile(mysql, tid)
+        company_owner = get_company_owner_by_tid(mysql, tid)
         if rid in [Role.TEAM_ADMIN.value, Role.TEAM_OWNER.value]:
             return render_template('company_profile.html', company_details=company_details,
                                    message=message, user_team=user_team, tid=tid,
                                    btnName="manage job postings", btnLk="/jobPostingsMgmt",
-                                   company_owner=company_owner, target_tid=target_tid)
+                                   company_owner=company_owner)
 
         return render_template('company_profile.html', company_details=company_details,
                                message=message, user_team=user_team, tid=tid,
